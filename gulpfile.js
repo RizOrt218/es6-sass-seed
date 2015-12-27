dfbd04c868fe616b8133ea1cfebafa9a8955cf16
RizOrt218 <rizhiafalen@gmail.com>
2015-12-26 15:17:40 -1000

    update task to handle multi js files

--- gulpfile.js
+++ gulpfile.js
@@ -12,8 +12,8 @@ gulp.task('sass', function () {
 //   gulp.watch('./sass/**/*.scss', ['sass']);
 // });
 
-gulp.task('default', function() {
-  return gulp.src('src/test.js')
+gulp.task('babel', function() {
+  return gulp.src('src/**/*.js')
     .pipe(babel({
       presets: ['es2015']
     }))
